a09e2978fecd63f3e2a9ad82b2a6de26df389bbd
Michelle Mounde <mitchmounde@gmail.com>
2022-06-21 16:58:35 +0300

    Cocktail menu and details loading on click to select

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const lookup = "lookup.php?"
 const random = "random.php?"
 const filter = "filter.php?"
 const list = "list.php?"
-const ingredient = "i="
+const ingr = "i="
 const name = "s="
 const firstLetter = "f="
-const ingredientID = "iid="
+const ingrID = "iid="
 const alcoholPresence = "a="
 const category = "c="
 const glass = "g="
@@ -16,45 +16,21 @@ const glass = "g="
 document.addEventListener("DOMContentLoaded", () => {
     getCocktail("11007")
     getCocktails()
-    
+    searchIngredient()
     
 
 })
-
-// Get co
-
-function getAlcohol(liquor) {
-    fetch(`${BASE_URL}/${filter}${ingredient}${liquor}`)
-    .then(resp => resp.json())
-    .then(data => {
-        const alcohol = data.drinks
-        loadAlcohol(liquor)
-    })
-    .catch(err => {throw err})
-}
-
-function loadAlcohol(name) {
-    const liquor = document.createElement("li")
-    const alcoholMenu = document.querySelector("#alcohol")
-
-    liquor.id = name
-    liquor.className = "alcohol-item"
-    liquor.textContent = name
-
-    alcoholMenu.append(liquor)
-
-}
 
 // Get and load cocktails based on alcohol
 function getCocktails() {
     const lis = document.querySelectorAll(".alcohol-item")
     lis.forEach(li => {
-        li.addEventListener("click", () => {
-            fetch(`${BASE_URL}/${filter}${ingredient}${li.textContent}`)
+        li.addEventListener("click", (e) => {
+
+            fetch(`${BASE_URL}/${filter}${ingr}${li.textContent}`)
             .then(resp => resp.json())
             .then(data => {
                 const cocktails = data.drinks
-                console.log(cocktails)
                 loadCocktails(cocktails)
             })
             .catch(err => {throw err})
@@ -63,24 +39,67 @@ function getCocktails() {
 }
 
 function loadCocktails(cocktails) {
+    const ol = document.querySelector("#cocktails")
+    if (ol.textContent === "") {
+        createCocktails(cocktails)
+    } else {
+        ol.textContent = ""
+        createCocktails(cocktails)
+    }
+}
+
+function createCocktails(cocktails) {
     cocktails.forEach(cocktail => {
         const li = document.createElement("li")
-        const ul = document.querySelector("#cocktails")
-
+        const ol = document.querySelector("#cocktails")
+           
         li.id = cocktail.idDrink
         li.className = "cocktails-item"
         li.textContent = cocktail.strDrink
+        
+        ol.append(li)
 
-        console.log(`li: ${li}`)
+        li.addEventListener("click", () => {
+            getCocktail(li.id)
+        })          
+    })
+}
 
-        ul.append("li")
+// Get and load cocktails based on search 
+function searchIngredient() {
+    const form = document.querySelector("#search-alcohol")
+    const searchIngr = form.querySelector("#alcohol-search")
+    const btn = form.querySelector("button")
+
+    btn.addEventListener("click", (e) => {
+        fetch(`${BASE_URL}/${filter}${ingr}${e.target.parentNode.searchIngr.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            e.preventDefault()
+            console.log(e.target.parentNode.searchIngr.value)
+            const cocktails = data.drinks
+            loadCocktails(cocktails)
+        })
+        .catch(err => {throw err})
+    })
+
+    searchIngr.addEventListener("input", (e) => {
+        fetch(`${BASE_URL}/${filter}${ingr}${e.target.value}`)
+            .then(resp => resp.json())
+            .then(data => {
+                e.preventDefault()
+                console.log(e.target.value)
+                const cocktails = data.drinks
+                loadCocktails(cocktails)
+            })
+            .catch(err => {throw err})
     })
 }
 
 
 // Get and load cocktail details to page on click
 function getCocktail(id) {
-    fetch(`${BASE_URL}/${lookup}${ingredient}${id}`)
+    fetch(`${BASE_URL}/${lookup}${ingr}${id}`)
     .then(resp => resp.json())
     .then(data => {
         const cocktail = data.drinks[0]
@@ -92,10 +111,25 @@ function getCocktail(id) {
 function loadCocktail(cocktail) {
     const details = document.querySelector("#cocktail-details")
     const img = details.querySelector("img")
-    const cocktailName = details.querySelector("#cocktail-name")
+    const name = details.querySelector("#cocktail-name")
+    const recipe = document.querySelector("#recipe")
+    const glass = document.querySelector("#glass")
+    const ingredients = document.querySelector("#ingredients")
+    
 
     img.src = `${cocktail.strDrinkThumb}/preview`
     img.alt = cocktail.strDrink
+    name.textContent = cocktail.strDrink.toUpperCase()
+    recipe.textContent = cocktail.strInstructions
+    glass.textContent = cocktail.strGlass
+    ingredients.textContent = ""
 
-    cocktailName.textContent = cocktail.strDrink
+    for (let key in cocktail) {
+        if ( key.startsWith("strIngredient") && cocktail[key] !== null && cocktail[key] !== "") {
+                const li = document.createElement("li")
+                li.className = "ingredient"
+                li.textContent = cocktail[key]
+                ingredients.append(li)
+        }
+    }
 }
